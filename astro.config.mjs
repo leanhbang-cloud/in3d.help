aac1b488304a66adfaa931b91bb658b57e05ea29
leanhbang-cloud <leanhang27983@gmail.com>
2026-07-27 17:05:17 +0700

    chore: remove cloudflare adapter to build standard static site into dist/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://in3d-help.pages.dev',
   output: 'static',
-  adapter: cloudflare({
-    remoteBindings: false,
-  }),
   integrations: [
     sitemap(),
   ],
